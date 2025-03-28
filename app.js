f4efcf31927e0806fbff6f406111b5159af21e97
Melksedeque <freelancer@melksedeque.com.br>
2025-03-28 13:25:55 -0300

    Criação inicial de função para pegar os elementos do HTML para o JS

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+  let quantidade = document.getElementById("quantidade").value;
+  let de = document.getElementById("de").value;
+  let ate = document.getElementById("ate").value;
+
+  alert(`Sorteando ${quantidade} números de ${de} a ${ate}`);
+}
+
+function reiniciar() {}
